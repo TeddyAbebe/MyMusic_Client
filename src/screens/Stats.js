cd9a1ed40b4ad40a309e3d86f8ab261e57e6aea1
Tewodros Abebe <teddyab729@gmail.com>
2023-03-10 14:46:14 +0300

    update

--- src/screens/Stats.js
+++ src/screens/Stats.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./home.css";
 
-
 function Stats({ songsList }) {
+  // Check if songsList is defined
+  if (!songsList) {
+    return <div>Loading...</div>;
+  }
+
   const totalSongs = songsList.length;
+  const totalDuration = songsList.reduce(
+    (total, song) => total + song.duration,
+    0
+  );
+
   const totalArtists = new Set(songsList.map((song) => song.artist)).size;
   const totalAlbums = new Set(songsList.map((song) => song.album)).size;
   const totalGenres = new Set(songsList.map((song) => song.genre)).size;
@@ -12,6 +21,7 @@ function Stats({ songsList }) {
     <div>
       <h2>Overall Statistics : </h2>
       <p>Total Songs : {totalSongs}</p>
+      <p>Total duration: {totalDuration} seconds</p>
       <p>Total Artists : {totalArtists}</p>
       <p>Total Albums : {totalAlbums}</p>
       <p>Total Genres : {totalGenres}</p>
